4699855a85ddaba356ce4e5f5c9b04b2c4800751
chemis-t <67217643+chemis-t@users.noreply.github.com>
2020-06-20 23:02:44 -0700

    Add files via upload

---
+++ open window.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+webdriver = webdriver.Chrome()  # type: WebDriver
+
+webdriver.get("http://www.goole.com")
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
